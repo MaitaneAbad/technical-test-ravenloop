33121b5acfab0025462fd5c1d07531f550101298
MaitaneAbad <maitaneabadconde@gmail.com>
2021-11-22 20:40:31 +0100

    add logout

--- src/js/00 main.js
+++ src/js/00 main.js
@@ -4,6 +4,7 @@ const password = document.querySelector('.js-inputPassword');
 const button = document.querySelector('.js-button');
 const container = document.querySelector('.js-container');
 const closeSection = document.getElementById('sectionClose');
+const logout = document.querySelector('.js-logout');
 
 let login = [];
 // let info = [];
@@ -12,8 +13,16 @@ let html = '';
 
 function list() {
   for (const malware of results) {
+    console.log(malware.scoreAnalysis);
+
     html += `<li id=${malware.id} class="page__main--sectionFile__sectionList--list hidden js-list">`;
-    html += `<h4 class="page__main--sectionFile__sectionList--list__name">Fichero: <span class="page__main--sectionFile__sectionList--list__name--span"> ${malware.fileName}</span></h4>`;
+    html += `<h4 class="page__main--sectionFile__sectionList--list__name">Fichero: <span class="page__main--sectionFile__sectionList--list__name--span"> ${
+      malware.fileName
+    }${
+      malware.scoreAnalysis <= '50'
+        ? '<i class="page__main--sectionFile__sectionList--list__name--span__iconClean fas fa-circle"></i>'
+        : '<i class="page__main--sectionFile__sectionList--list__name--span__iconDanger fas fa-circle"></i>'
+    }</span></h4>`;
     html += `<p class="page__main--sectionFile__sectionList--list__name">Md5:
      <span class="page__main--sectionFile__sectionList--list__name--span">${malware.hashmd5}</span></p>`;
     html += `<p class="page__main--sectionFile__sectionList--list__name">Fecha de inserción: <span class="page__main--sectionFile__sectionList--list__name--span">${malware.insertionDate}</span></p>`;
@@ -29,7 +38,7 @@ function list() {
 function loginCredentials() {
   if (username.value === login.username && password.value === login.password) {
     html = '';
-
+    // html += `<form><input class="js-logout" type="button" value="cerrar sesión"/> </form>`;
     list();
     closeSection.classList.add('hidden');
     username.value = '';
@@ -41,6 +50,7 @@ function loginCredentials() {
     html += `<div> Usuario o contraseña incorrecta </div>`;
   }
   container.innerHTML = html;
+
   listenList();
 }
 
@@ -108,7 +118,11 @@ function back() {
     back.addEventListener('click', handlerBack);
   }
 }
+// function handlerLogout() {
+//   const logout = document.querySelector('.js-logout');
 
+//   location.reload();
+// }
 function listenList() {
   const listMalware = document.querySelectorAll('.js-list');
   for (const listClick of listMalware) {
@@ -135,5 +149,8 @@ api();
 function handlerButton() {
   loginCredentials();
 }
-
+function handlerLogout() {
+  location.reload();
+}
 button.addEventListener('click', handlerButton);
+logout.addEventListener('click', handlerLogout);

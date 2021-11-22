337a94e8fb7b2ad30f13776015acce5fa029ec38
MaitaneAbad <maitaneabadconde@gmail.com>
2021-11-22 15:40:29 +0100

    change html file

--- src/js/00 main.js
+++ src/js/00 main.js
@@ -3,7 +3,8 @@ const username = document.querySelector('.js-inputUser');
 const password = document.querySelector('.js-inputPassword');
 const button = document.querySelector('.js-button');
 const container = document.querySelector('.js-container');
-// const js = document.querySelector('.jsa');
+const closeSection = document.getElementById('sectionClose');
+
 let login = [];
 // let info = [];
 let results = [];
@@ -22,16 +23,17 @@ function list() {
   container.innerHTML = html;
 
   listenList();
-  handleList();
 }
 
 //funcion para loguearme
 function loginCredentials() {
   if (username.value === login.username && password.value === login.password) {
     html = '';
+
+    list();
+    closeSection.classList.add('hidden');
     username.value = '';
     password.value = '';
-    list();
   } else {
     username.value = '';
     password.value = '';
@@ -39,17 +41,23 @@ function loginCredentials() {
     html += `<div> Usuario o contraseña incorrecta </div>`;
   }
   container.innerHTML = html;
+  listenList();
 }
 
 // Eventos para al clicar en un malware que me quite el listado y me aparezca solo los datos del clicado
 function handleList(ev) {
   const selected = ev.currentTarget.id;
   const infoSelected = results[selected - 1];
+
   html = ' ';
   html +=
     '<article class="page__main--sectionFile__sectionInfo--info"> <h3 class="page__main--sectionFile__sectionInfo--info__title">Información </h3>';
   html += `<p class="page__main--sectionFile__sectionInfo--info__name"> Nombre completo: ${infoSelected.fileNameComplete} </p>`;
-  html += `<p class="page__main--sectionFile__sectionInfo--info__extension">Extensión: ${infoSelected.fileExtension} </p>`;
+  html += `<p class="page__main--sectionFile__sectionInfo--info__extension">Extensión: ${
+    infoSelected.fileExtension === 'PDF'
+      ? 'PDF <i class="fas fa-file-pdf"></i>'
+      : ''
+  } </p>`;
   html += `<p class="page__main--sectionFile__sectionInfo--info__size"> Tamañaño: ${infoSelected.fileSize} </p>`;
   html += `<ul class="page__main--sectionFile__sectionInfo--info__list"> <li class="page__main--sectionFile__sectionInfo--info__list--hash"> Hash md5: ${infoSelected.hash.md5}<li> `;
   html += `<li class="page__main--sectionFile__sectionInfo--info__list--hash"> Hash sha256: ${infoSelected.hash.sha256}</li> `;
@@ -60,10 +68,26 @@ function handleList(ev) {
   html += `<ul class="page__main--sectionFile__sectionInfo--info__list"> Listado de ficheros generados: <li class="page__main--sectionFile__sectionInfo--info__list--file">${infoSelected.fileList.fileOne}</li>`;
   html += ` <li class="page__main--sectionFile__sectionInfo--info__list--file">${infoSelected.fileList.fileTwo}</li>`;
   html += ` <li class="page__main--sectionFile__sectionInfo--info__list--file">${infoSelected.fileList.fileThree}</li></ul>`;
-  html += `<ul class="page__main--sectionFile__sectionInfo--info__list"> Listado de antivirus: <li class="page__main--sectionFile__sectionInfo--info__list--antivirus">Avast: ${infoSelected.antivirusList.avast}</li>`;
-  html += `<li class="page__main--sectionFile__sectionInfo--info__list--antivirus">McAfree: ${infoSelected.antivirusList.mcafree}</li>`;
-  html += `<li class="page__main--sectionFile__sectionInfo--info__list--antivirus">BitDefender: ${infoSelected.antivirusList.bitdefender}</li>`;
-  html += `<li class="page__main--sectionFile__sectionInfo--info__list--antivirus">Panda: ${infoSelected.antivirusList.panda}</li>`;
+  html += `<ul class="page__main--sectionFile__sectionInfo--info__list"> Listado de antivirus: <li class="page__main--sectionFile__sectionInfo--info__list--antivirus">Avast: ${
+    infoSelected.antivirusList.avast === 'clean'
+      ? 'Limpio <i class="fas fa-check"></i>'
+      : 'Peligroso <i class="fas fa-exclamation-triangle"></i></i>'
+  }</li>`;
+  html += `<li class="page__main--sectionFile__sectionInfo--info__list--antivirus">McAfree: ${
+    infoSelected.antivirusList.mcafree === 'clean'
+      ? 'Limpio <i class="fas fa-check"></i>'
+      : 'Peligroso <i class="fas fa-exclamation-triangle"></i></i>'
+  }</li>`;
+  html += `<li class="page__main--sectionFile__sectionInfo--info__list--antivirus">BitDefender: ${
+    infoSelected.antivirusList.bitdefender === 'clean'
+      ? 'Limpio <i class="fas fa-check"></i>'
+      : 'Peligroso <i class="fas fa-exclamation-triangle"></i></i>'
+  }</li>`;
+  html += `<li class="page__main--sectionFile__sectionInfo--info__list--antivirus">Panda: ${
+    infoSelected.antivirusList.panda === 'clean'
+      ? 'Limpio <i class="fas fa-check"></i>'
+      : 'Peligroso <i class="fas fa-exclamation-triangle"></i></i>'
+  }</li>`;
   html += ` </ul><form><input
   class="js-buttonBack"
   type="button"

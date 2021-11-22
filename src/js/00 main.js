b547afa4b2b90adf7542da41dac7a08c5c3a45fa
MaitaneAbad <maitaneabadconde@gmail.com>
2021-11-22 12:08:01 +0100

    add style header and footer

--- src/js/00 main.js
+++ src/js/00 main.js
@@ -37,23 +37,24 @@ function handleList(ev) {
   const selected = ev.currentTarget.id;
   const infoSelected = results[selected - 1];
   html = ' ';
-  html += '<div> Información ';
-  html += `<p> Nombre completo: ${infoSelected.fileNameComplete} </p>`;
-  html += `<p>Extensión: ${infoSelected.fileExtension} </p>`;
-  html += `<p> Tamañaño: ${infoSelected.fileSize} </p>`;
-  html += `<ul> <li> Hash md5: ${infoSelected.hash.md5}<li> `;
-  html += `<li> Hash sha256: ${infoSelected.hash.sha256}</li> `;
-  html += `<li> Hash sha-1: ${infoSelected.hash.sha1}</li></ul> `;
-  html += `<p> Dirección <span>ip</span> a las que se ha conectado: ${infoSelected.ip} </p>
+  html +=
+    '<article class="page__main--sectionFile__sectionInfo--info"> <h3 class="page__main--sectionFile__sectionInfo--info__title">Información </h3>';
+  html += `<p class="page__main--sectionFile__sectionInfo--info__name"> Nombre completo: ${infoSelected.fileNameComplete} </p>`;
+  html += `<p class="page__main--sectionFile__sectionInfo--info__extension">Extensión: ${infoSelected.fileExtension} </p>`;
+  html += `<p class="page__main--sectionFile__sectionInfo--info__size"> Tamañaño: ${infoSelected.fileSize} </p>`;
+  html += `<ul class="page__main--sectionFile__sectionInfo--info__list"> <li class="page__main--sectionFile__sectionInfo--info__list--hash"> Hash md5: ${infoSelected.hash.md5}<li> `;
+  html += `<li class="page__main--sectionFile__sectionInfo--info__list--hash"> Hash sha256: ${infoSelected.hash.sha256}</li> `;
+  html += `<li class="page__main--sectionFile__sectionInfo--info__list--hash"> Hash sha-1: ${infoSelected.hash.sha1}</li></ul> `;
+  html += `<p class="page__main--sectionFile__sectionInfo--info__ip"> Dirección <span class="page__main--sectionFile__sectionInfo--info__ip--span">ip</span> a las que se ha conectado: ${infoSelected.ip} </p>
   `;
-  html += `<p> Puntuación obtenida tras el análisis: ${infoSelected.scoreAnalysis}</p>`;
-  html += `<ul> Listado de ficheros generados: <li>${infoSelected.fileList.fileOne}</li>`;
-  html += ` <li>${infoSelected.fileList.fileTwo}</li>`;
-  html += ` <li>${infoSelected.fileList.fileThree}</li></ul>`;
-  html += `<ul> Listado de antivirus: <li>Avast: ${infoSelected.antivirusList.avast}</li>`;
-  html += `<ul> Listado de antivirus: <li>McAfree: ${infoSelected.antivirusList.mcafree}</li>`;
-  html += `<ul> Listado de antivirus: <li>BitDefender: ${infoSelected.antivirusList.bitdefender}</li>`;
-  html += `<ul> Listado de antivirus: <li>Panda: ${infoSelected.antivirusList.panda}</li></ul>`;
+  html += `<p class="page__main--sectionFile__sectionInfo--info__score"> Puntuación obtenida tras el análisis: ${infoSelected.scoreAnalysis}</p>`;
+  html += `<ul class="page__main--sectionFile__sectionInfo--info__list"> Listado de ficheros generados: <li class="page__main--sectionFile__sectionInfo--info__list--file">${infoSelected.fileList.fileOne}</li>`;
+  html += ` <li class="page__main--sectionFile__sectionInfo--info__list--file">${infoSelected.fileList.fileTwo}</li>`;
+  html += ` <li class="page__main--sectionFile__sectionInfo--info__list--file">${infoSelected.fileList.fileThree}</li></ul>`;
+  html += `<ul class="page__main--sectionFile__sectionInfo--info__list"> Listado de antivirus: <li class="page__main--sectionFile__sectionInfo--info__list--antivirus">Avast: ${infoSelected.antivirusList.avast}</li>`;
+  html += `<li class="page__main--sectionFile__sectionInfo--info__list--antivirus">McAfree: ${infoSelected.antivirusList.mcafree}</li>`;
+  html += `<li class="page__main--sectionFile__sectionInfo--info__list--antivirus">BitDefender: ${infoSelected.antivirusList.bitdefender}</li>`;
+  html += `<li class="page__main--sectionFile__sectionInfo--info__list--antivirus">Panda: ${infoSelected.antivirusList.panda}</li></ul></article>`;
   container.innerHTML = html;
 }
 
